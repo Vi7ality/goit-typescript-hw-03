7a6d32067b27c8a6c39b4c18bcbeb59e68d79bba
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-01-21 23:57:16 +0200

    task 2 started

--- src/2.ts
+++ src/2.ts
@@ -14,16 +14,31 @@
 
 */
 
-class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+// class Employee {
+//   // Заповніть модифікатори доступу
+//   name: string;
+//   department: string;
+//   salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+//   constructor(name: string, department: string, salary: number) {
+//     this.name = name;
+//     this.department = department;
+//     this.salary = salary;
+//   }
+
+//   getEmployeeDetails() {
+//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+//   }
+// }
+
+// class Manager extends Employee {
+//   // Реалізуйте конструктор та збільшіть salary на 10000
+// }
+
+class Employee {
+
+  constructor(public name: string, private department: string, protected salary: number) {
+
   }
 
   getEmployeeDetails() {
@@ -33,7 +48,11 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(salary: number) {
+    super(1000, salary);
+  }
 }
+
 
 
 export {};
